9f2d9da4390228b014ad982092fbc8baf9473aa2
오진수 <tankboy0913@gmail.com>
2022-03-05 22:18:54 +0900

    Move the app components

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,8 +1,8 @@
 <script>
   import "../app.css"
-  import AppHeader from "src/components/app_header.svelte";
-  import AppMain from "src/components/app_main.svelte";
-  import AppFooter from "src/components/app_footer.svelte";
+  import AppHeader from "src/components/app/app_header.svelte";
+  import AppMain from "src/components/app/app_main.svelte";
+  import AppFooter from "src/components/app/app_footer.svelte";
 </script>
 
 <AppHeader />
